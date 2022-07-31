40b16611cad7d8a26114ed39c46c3fe3c3f87798
aayush-aryan <log2aayusharyan@gmail.com>
2022-08-01 02:10:30 +0530

    Creating BST and adding elements

---
+++ Section4BinarySearchTree/BinarySearchTree.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Section4BinarySearchTree
+{
+    class BinarySearchTree<T> where T : IComparable
+    {
+        Node<T> Root;
+        Node<T> Current;
+        /// <summary>
+        /// method for Inserting data in BST
+        /// </summary>
+        /// <param name="data"></param>
+        public void InsertData(T data)
+        {
+            //Checking rootNode null or not.
+            if (Root == null)
+            {
+                this.Root = new Node<T>(data);
+                this.Current = Root;
+                return;
+            }
+            if (this.Current.data.CompareTo(data) > 0) //If data is less than data in root
+                                                       //then current position node will be point to left.
+            {
+                if (this.Current.leftNode == null) //Checking left node is null or not.
+                {
+                    this.Current.leftNode = new Node<T>(data);
+                    this.Current = Root;
+                }
+                else
+                {
+                    this.Current = this.Current.leftNode;
+                    InsertData(data);
+                }
+            }
+            else
+            {
+                if (this.Current.rightNode == null) // Checking right node is null or not.
+                {
+                    this.Current.rightNode = new Node<T>(data);
+                    this.Current = Root;
+                }
+                else
+                {
+                    this.Current = this.Current.rightNode;
+                    InsertData(data);
+                }
+            }
+        }
+        /// <summary>
+        /// method for getting rootNode.
+        /// </summary>
+        /// <returns></returns>
+        public Node<T> GetRoot()
+        {
+            return this.Root;
+        }
+        /// <summary>
+        /// method for displaying element or nodeData in bst
+        /// </summary>
+        /// <param name="node"></param>
+        public void Display(Node<T> node)
+        {
+            if (node != null)
+            {
+                Display(node.leftNode);
+                Console.WriteLine("Element in BST tree is: " + node.data);
+                Display(node.rightNode);
+            }
+        }
+    }
+}
--- Section4BinarySearchTree/Program.cs
+++ Section4BinarySearchTree/Program.cs
@@ -7,6 +7,11 @@ namespace Section4BinarySearchTree
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to section-4 BinarySearchTree!");
+            BinarySearchTree<int> binarySearchTree = new BinarySearchTree<int>();
+            binarySearchTree.InsertData(56);
+            binarySearchTree.InsertData(30);
+            binarySearchTree.InsertData(70);
+            binarySearchTree.Display(binarySearchTree.GetRoot());
         }
     }
 }
